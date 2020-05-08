3de304350d9abf9943ffc52428e8449a4c834bb2
DATA8320-31 <DATA8320-31@DESKTOP-8P0VGKU>
2020-05-08 11:16:43 +0900

    신규프로젝트 올립니다.

---
+++ jivaPrj/src/jivaPrj/hello_jimin.java
@@ -0,0 +1,10 @@
+package jivaPrj;
+
+public class hello_jimin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("하이 모두들 안녕? 나는 이하늬다! 이하늬다!");
+	}
+
+}
