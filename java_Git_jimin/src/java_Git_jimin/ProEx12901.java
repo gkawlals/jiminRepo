e7a9205ad01894bdfb68dad668b42c70f09443e2
함지민 <hua4187@naver.com>
2020-06-01 12:58:24 +0900

    0601잔디(프로그래머스 문제 연습)

--- java_Git_jimin/src/java_Git_jimin/ProEx12901.java
+++ java_Git_jimin/src/java_Git_jimin/ProEx12901.java
@@ -1,10 +1,24 @@
 package java_Git_jimin;
-
+import java.util.*;
 public class ProEx12901 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int mon;
+		int day;
+		String [] answer = {"SUN,MON,TUE,WED,THU,FRI,SAT"};
+		
+		mon = sc.nextInt();
+		day = sc.nextInt();
+		switch (mon % 12) {
+		case 11 : 
+			switch(day % 7) {
+			case 0 : 
+			}
+		}
+		
 
 	}
-
 }
+	
