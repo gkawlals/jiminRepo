e964fb1436c0b689ae41e6e1275bdae45e325572
함지민 <hua4187@naver.com>
2020-06-02 17:23:30 +0900

    0602 git 잔디

---
+++ java_Git_jimin/src/java_Git_jimin/back_note02.java
@@ -0,0 +1,29 @@
+package java_Git_jimin;
+
+import java.util.*;
+
+public class back_note02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+		if(a >= 90) {
+			System.out.println("A");
+		}
+		else if(a >= 80 ) {
+			System.out.println("B");
+		}
+		else if(a >= 70 ) {
+			System.out.println("C");
+		}
+		else if(a >= 60 ) {
+			System.out.println("D");
+		}
+		else if(a <= 60) {
+			System.out.println("F");
+		}
+		
+	}
+
+}
