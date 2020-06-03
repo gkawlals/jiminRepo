b2d5bed9458fb4e7c2425f16288e0193618e8b18
함지민 <hua4187@naver.com>
2020-06-03 20:51:14 +0900

    0603 노트

---
+++ java_Git_jimin/src/java_Git_jimin/bacck_note05.java
@@ -0,0 +1,19 @@
+package java_Git_jimin;
+import java.util.*;
+public class bacck_note05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		
+		a = sc.nextInt();
+		int i;
+		for ( i = a; i <= a; i++) {
+			for(int k = 1; k <= 9; k++) {
+				System.out.printf("%d * %d = %d \n",i,k,i*k);	
+			}
+			
+		}
+	}
+
+}
