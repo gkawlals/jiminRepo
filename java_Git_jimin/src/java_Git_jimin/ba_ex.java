cc27ee2658330b1bd57ea76cdcd482560c5b2632
함지민 <hua4187@naver.com>
2020-06-11 18:52:44 +0900

    0611git

---
+++ java_Git_jimin/src/java_Git_jimin/ba_ex.java
@@ -0,0 +1,29 @@
+package java_Git_jimin;
+import java.util.*;
+public class ba_ex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		int d;
+		
+		int haq;
+		
+		System.out.printf("1번째 숫자를 입력하세요");
+		a = sc.nextInt();
+		System.out.printf("2번째 숫자를 입력하세요");
+		b = sc.nextInt();
+		System.out.printf("3번째 숫자를 입력하세요");
+		c = sc.nextInt();
+		System.out.printf("4번째 숫자를 입력하세요");
+		d = sc.nextInt();
+		
+		haq = a +b+c+d;
+		
+		System.out.printf(" 합  : = %d", haq);
+	}
+
+}
